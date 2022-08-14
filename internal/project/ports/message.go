0d2aef302e24c8b755a32b2745b785a3a92e2ed6
Anucha Mai <anucha.mai@truedigital.com>
2022-08-14 17:14:34 +0700

    init ports for project

---
+++ internal/project/ports/message.go
@@ -0,0 +1,7 @@
+package ports
+
+import "context"
+
+type MessagePublisher interface {
+	Publish(ctx context.Context, topic string, b []byte)
+}
---
+++ internal/project/ports/repository.go
@@ -0,0 +1,15 @@
+package ports
+
+import (
+	"context"
+	"github.com/monopeelz/linear-avocado/internal/project/models"
+)
+
+type ProjectRepository interface {
+	Create(ctx context.Context, project models.Project) (models.Project, error)
+	Update(ctx context.Context, project models.Project) (models.Project, error)
+	Delete(ctx context.Context, id string) error
+	GetAll(ctx context.Context) ([]models.Project, error)
+	GetByID(ctx context.Context, id string) (models.Project, error)
+	GetJobs(ctx context.Context, id string) ([]models.Job, error)
+}
