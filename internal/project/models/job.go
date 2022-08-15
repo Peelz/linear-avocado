d44442de2b0dab2e6c54dd9e1c97041254cd63b5
Anucha Mai <anucha.mai@truedigital.com>
2022-08-15 23:00:38 +0700

    project: update Entity method

--- internal/project/models/job.go
+++ internal/project/models/job.go
@@ -1,37 +1,54 @@
 package models
 
 import (
-	"github.com/monopeelz/linear-avocado/internal/pkg/proj"
+	"github.com/google/uuid"
 	"github.com/monopeelz/linear-avocado/internal/project/entity"
 	"time"
 )
+
+type JobStatus string
+
+func (j JobStatus) String() string {
+	return string(j)
+}
+
+const (
+	Queued     JobStatus = "Queued"
+	InProgress JobStatus = "InProgress"
+	Success    JobStatus = "Success"
+	Failure    JobStatus = "Failure"
+)
+
+type JobDetail struct {
+	Findings map[string]interface{} `json:"findings"`
+	Errors   map[string]interface{} `json:"errors"`
+}
 
 type Job struct {
 	// ID: Job ID
 	ID int `bun:"id,pk,autoincrement" json:"-"`
 	// UUID: Global ID
-	UUID string `bun:"uuid" json:"uuid"`
+	UUID uuid.UUID `bun:"uuid" json:"uuid"`
 	// ProjectID: Project ID use for RDBMS
-	ProjectID int `bun:"project_id" json:"-"`
+	ProjectID int `json:"-"`
 	// CreatedAt: timestamp
-	CreatedAt *time.Time
+	CreatedAt *time.Time `json:"createdAt"`
 	// StartedAt: timestamp
-	StartedAt *time.Time
+	StartedAt *time.Time `json:"startedAt"`
 	// FinishedAt: timestamp
-	FinishedAt *time.Time
+	FinishedAt *time.Time `json:"finishedAt"`
 	// Detail: output from processing
-	Detail map[string]interface{} `bun:"type:json"`
-	// StatusID: "Queued" | "In Progress" | "Success" | "Failure"
-	StatusID proj.ScanningStatus
+	Detail map[string]interface{} `bun:"type:json" json:"detail"`
+	// StatusID: "Queued" | "In InProgress" | "Success" | "Failure"
+	Status JobStatus `bun:"type:string" json:"status"`
 }
 
 func (j Job) Entity() entity.Job {
 	return entity.Job{
-		ID:         j.ID,
-		UUID:       j.UUID,
+		UUID:       j.UUID.String(),
 		CreatedAt:  j.CreatedAt,
 		StartedAt:  j.CreatedAt,
 		FinishedAt: j.FinishedAt,
-		Status:     j.StatusID.String(),
+		Status:     j.Status.String(),
 	}
 }
