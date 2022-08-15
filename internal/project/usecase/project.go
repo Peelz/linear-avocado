bde6bba460d076cc8dadbcad6b46334026b950b6
Anucha Mai <anucha.mai@truedigital.com>
2022-08-15 23:01:17 +0700

    project: change receiver to pointer, update scan method to create job

--- internal/project/usecase/project.go
+++ internal/project/usecase/project.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/google/uuid"
+	"github.com/monopeelz/linear-avocado/internal/project/entity"
 	"github.com/monopeelz/linear-avocado/internal/project/models"
 	"github.com/monopeelz/linear-avocado/internal/project/ports"
+	"go.uber.org/zap"
 )
 
 // ProjectUseCase Interface for Project business logic
@@ -15,59 +17,77 @@ type ProjectUseCase interface {
 	Delete(ctx context.Context, i string) error
 	All(ctx context.Context) ([]models.Project, error)
 	Get(ctx context.Context, i string) (models.Project, error)
-	Scan(ctx context.Context, i string) error
+	Scan(ctx context.Context, i string) (entity.Job, error)
 }
 
 type projectUseCase struct {
-	rp  ports.ProjectRepository
-	msg ports.MessagePublisher
+	rp     ports.ProjectRepository
+	msg    ports.MessagePublisher
+	logger *zap.Logger
 }
 
-func (p projectUseCase) Scan(ctx context.Context, i string) error {
+func (p *projectUseCase) Scan(ctx context.Context, i string) (entity.Job, error) {
 	proj, err := p.rp.GetByID(ctx, i)
 	if err != nil {
-		return err
-	}
-	b, err := json.Marshal(proj)
-	if err != nil {
-		return err
-	}
-	err = p.msg.Publish(ctx, "scan-project", b)
-	if err != nil {
-		return err
+		return entity.Job{}, err
 	}
 
-	return nil
+	_job, err := p.rp.CreateJobFromProject(ctx, proj)
+	job := _job.Entity()
+	job.Project = proj.Entity()
+	if err != nil {
+		return entity.Job{}, err
+	}
+	b, err := json.Marshal(job)
+	if err != nil {
+		p.logger.Error("", zap.Error(err))
+		return entity.Job{}, err
+	}
+
+	go func() {
+		err = p.msg.Publish(ctx, "scan-project", b)
+		if err != nil {
+			p.logger.Error("", zap.Error(err))
+		}
+
+	}()
+
+	return job, nil
 }
 
-func (p projectUseCase) Create(ctx context.Context, i models.Project) (models.Project, error) {
+func (p *projectUseCase) Create(ctx context.Context, i models.Project) (models.Project, error) {
 	i.UUID, _ = uuid.NewUUID()
 	return p.rp.Create(ctx, i)
 }
 
-func (p projectUseCase) Update(ctx context.Context, i models.UpdateProject) (models.Project, error) {
+func (p *projectUseCase) Update(ctx context.Context, i models.UpdateProject) (models.Project, error) {
 	return p.rp.Update(ctx, i)
 }
 
-func (p projectUseCase) Delete(ctx context.Context, i string) error {
+func (p *projectUseCase) Delete(ctx context.Context, i string) error {
 	return p.rp.DeleteBytProjectID(ctx, i)
 }
 
-func (p projectUseCase) All(ctx context.Context) ([]models.Project, error) {
+func (p *projectUseCase) All(ctx context.Context) ([]models.Project, error) {
 	return p.rp.GetAll(ctx)
 }
 
-func (p projectUseCase) Get(ctx context.Context, i string) (models.Project, error) {
+func (p *projectUseCase) Get(ctx context.Context, i string) (models.Project, error) {
 	return p.rp.GetByID(ctx, i)
 }
 
-func (p projectUseCase) GetJob(ctx context.Context, i string) ([]models.Job, error) {
+func (p *projectUseCase) GetJob(ctx context.Context, i string) ([]models.Job, error) {
 	return p.rp.GetJobs(ctx, i)
 }
 
-func NewProjectUseCase(rp ports.ProjectRepository, msg ports.MessagePublisher) ProjectUseCase {
+func NewProjectUseCase(
+	rp ports.ProjectRepository,
+	msg ports.MessagePublisher,
+	logger *zap.Logger,
+) ProjectUseCase {
 	return &projectUseCase{
 		rp,
 		msg,
+		logger,
 	}
 }
